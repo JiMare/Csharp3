fc233457ed0690ab7f075cb78ab43bffa9663cd5
JiMare <zoa@seznam.cz>
2025-09-26 23:10:22 +0200

    fizzbuzz

---
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+
+Console.WriteLine("Hello, World!");
+FizzBuzz fizzbuzz = new FizzBuzz();
+fizzbuzz.CountTo(16);
